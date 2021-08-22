2ea7f948100d049ea6e501b0d414d6fa80686413
fiatjaf <fiatjaf@gmail.com>
2021-08-22 11:11:54 -0300

    websocket.

--- eclair.go
+++ eclair.go
@@ -7,17 +7,30 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"mime/multipart"
 	"net/http"
 	"strings"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/tidwall/gjson"
 )
 
 type Client struct {
 	Host     string
 	Password string
+}
+
+func (c Client) baseURL() string {
+	if strings.HasPrefix(c.Host, "http") {
+		return c.Host
+	}
+	return "http://" + c.Host
+}
+
+func (c Client) authorizationHeader() string {
+	return "Basic " + base64.StdEncoding.EncodeToString([]byte(":"+c.Password))
 }
 
 type ErrorResponse struct {
@@ -27,15 +40,14 @@ type ErrorResponse struct {
 type Params map[string]interface{}
 
 func (c *Client) Call(method string, data map[string]interface{}) (gjson.Result, error) {
-	r, err := http.NewRequest("POST", c.Host+"/"+method, nil)
+	r, err := http.NewRequest("POST", c.baseURL()+"/"+method, nil)
 	if err != nil {
 		return gjson.Result{},
 			fmt.Errorf("error creating http request to %s: %w", c.Host, err)
 	}
 
 	r.Header.Set("Accept", "application/json")
-	r.Header.Set("Authorization",
-		"Basic "+base64.StdEncoding.EncodeToString([]byte(":"+c.Password)))
+	r.Header.Set("Authorization", c.authorizationHeader())
 
 	if data != nil {
 		body := &bytes.Buffer{}
@@ -98,4 +110,29 @@ func (c *Client) Call(method string, data map[string]interface{}) (gjson.Result,
 
 	// now that we know the response is good json, we use gjson
 	return gjson.ParseBytes(b), nil
+}
+
+func (c *Client) Websocket() (<-chan gjson.Result, error) {
+	url := strings.Replace(c.baseURL(), "http", "ws", 1) + "/ws"
+	conn, _, err := websocket.DefaultDialer.Dial(url, http.Header{
+		"Authorization": {c.authorizationHeader()},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to open websocket connection: %w", err)
+	}
+
+	messages := make(chan gjson.Result)
+	go func() {
+		defer close(messages)
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("eclair ws read error:", err)
+				return
+			}
+			messages <- gjson.ParseBytes(message)
+		}
+	}()
+
+	return messages, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fiatjaf/eclair-go
 
 go 1.16
 
-require github.com/tidwall/gjson v1.8.1
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/tidwall/gjson v1.8.1
+)
